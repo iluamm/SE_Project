3282ea4ce8c1d8acf309b3e4aeff719134a02374
NDstayaside <saruda465@gmail.com>
2023-03-15 23:22:06 +0700

    Add profile icon and edit style

--- navbar.php
+++ navbar.php
@@ -3,23 +3,6 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Inter&family=Noto+Sans+Thai&display=swap" rel="stylesheet">
 <style>
-.navbarmenu{
-    display: flex;
-    height: 100%;
-    align-items: center;
-    justify-content: flex-start;
-    gap: 25%;
-    white-space: nowrap;
-}
-
-.navbaruserlist{
-    display: flex;
-    height: 100%;
-    align-items: center;
-    justify-content: flex-end;
-    gap: 25%;
-    white-space: nowrap;
-}
 
 .navbar{
     width: auto;
@@ -31,6 +14,36 @@
     align-items: center;
 }
 
+.navbarmenu{
+    display: flex;
+    height: 100%;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 20%;
+    white-space: nowrap;
+}
+
+.navbaruserlist{
+    display: flex;
+    height: 100%;
+    align-items: center;
+    justify-content: flex-end;
+    gap: 20%;
+    white-space: nowrap;
+}
+
+.profileicon{
+    display: flex;
+    height: 100%;
+    align-items: center;
+    gap: 10%;
+}
+
+a:link, a:visited {
+    color: black;
+    text-decoration: none;
+}
+
 </style>
 <nav class="navbar">
     <div class="navbarmenu">
@@ -40,9 +53,11 @@
         <div>อัดรูป</div> 
     </div>
     <div class="navbaruserlist">
-        <div>
-            <?php if(!isset($_SESSION['user_username'])) { ?><a href="login.php">Log in / Sign in </a><?php } ?></div>
+        <div class="profileicon">
+            <img src="\image\Profile.png" width="30rem">
+            <?php if(!isset($_SESSION['user_username'])) { ?><a href="login.php">Log in / Sign in </a><?php } ?>
+        </div>
         <div><?php if(isset($_SESSION['user_username'])) { ?>Hello! <?php echo $_SESSION['user_username']; } ?></div>
-        <div><?php if(isset($_SESSION['user_username'])) { ?><a href="logout.php">ออกจากระบบ</a><?php } ?></div>
+        <div><?php if(isset($_SESSION['user_username'])) { ?><a href="logout.php" class="linkstyle">ออกจากระบบ</a><?php } ?></div>
     </div>
 </nav>
