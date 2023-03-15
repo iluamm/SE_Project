dcbef14d689a492393185af605a5bead0aa74cfe
NDstayaside <saruda465@gmail.com>
2023-03-16 01:06:18 +0700

    Add menu list

---
+++ menulist.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <?php
+    include("navbar.php")
+    ?>
+    <div class="contextbox">
+        <h3 align="center">อัลบั้มสอด</h3>
+
+        <div class="menuitemlist">
+            <div class="menueachitemlist">
+                <div class="picturemenu">
+                    <img src="album\elephant.jpg">
+                </div>
+                <p>3x2 นิ้ว</p>
+                <p class="graytext">จำนวน 64 รูป ราคา 279 บาท</p>
+            </div>
+
+            <div class="menueachitemlist">
+                <div class="picturemenu">
+                    <img src="album\elephant.jpg">
+                </div>
+                <p>3x2 นิ้ว</p>
+                <p class="graytext">จำนวน 64 รูป ราคา 279 บาท</p>
+            </div>
+
+            <div class="menueachitemlist">
+                <div class="picturemenu">
+                    <img src="album\elephant.jpg">
+                </div>
+                <p>3x2 นิ้ว</p>
+                <p class="graytext">จำนวน 64 รูป ราคา 279 บาท</p>
+            </div>
+
+            <div class="menueachitemlist">
+                <div class="picturemenu">
+                    <img src="album\elephant.jpg">
+                </div>
+                <p>3x2 นิ้ว</p>
+                <p class="graytext">จำนวน 64 รูป ราคา 279 บาท</p>
+            </div>
+
+            <div class="menueachitemlist">
+                <div class="picturemenu">
+                    <img src="album\elephant.jpg">
+                </div>
+                <p>3x2 นิ้ว</p>
+                <p class="graytext">จำนวน 64 รูป ราคา 279 บาท</p>
+            </div>
+
+            <div class="menueachitemlist">
+                <div class="picturemenu">
+                    <img src="album\elephant.jpg">
+                </div>
+                <p>3x2 นิ้ว</p>
+                <p class="graytext">จำนวน 64 รูป ราคา 279 บาท</p>
+            </div>
+
+            <div class="menueachitemlist">
+                <div class="picturemenu">
+                    <img src="album\elephant.jpg">
+                </div>
+                <p>3x2 นิ้ว</p>
+                <p class="graytext">จำนวน 64 รูป ราคา 279 บาท</p>
+            </div>
+
+            <div class="menueachitemlist">
+                <div class="picturemenu">
+                    <img src="album\elephant.jpg">
+                </div>
+                <p>3x2 นิ้ว</p>
+                <p class="graytext">จำนวน 64 รูป ราคา 279 บาท</p>
+            </div>
+
+        </div>
+    </div>
+    
+</body>
+<?php
+ include("footer.php")
+?>
+</html>
