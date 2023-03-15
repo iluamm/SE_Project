968a970808acb13cf512c570ae5ec8a7ab24d212
iluamm <118557520+iluamm@users.noreply.github.com>
2023-03-16 01:05:54 +0700

    add admin page

---
+++ checkordersent.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+require_once 'connect.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+
+<?php
+include("navbar-admin.php") //navbar
+?>
+
+<h2 align="center">รายการสินค้าที่จัดส่ง</h2>
+<table border="1" align="center">
+    <tr>
+        <th>เลขที่ออเดอร์</th>
+        <th>เวลา</th>
+        <th colspan="2"></th>
+    </tr>
+    <tr>
+        <td>xxxxx</td>
+        <td>xx/xx/xxxx</td>
+        <td>ยกเลิก</td>
+        <td>รายละเอียด</td>
+    </tr>
+    <tr>
+        <td>xxxxx</td>
+        <td>xx/xx/xxxx</td>
+        <td>ยกเลิก</td>
+        <td>รายละเอียด</td>
+    </tr>
+</table>
+
+
+
+
+</body>
+<?php
+ include("footer.php")
+?>
+</html>
---
+++ navbar-admin.php
@@ -0,0 +1,69 @@
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter&family=Noto+Sans+Thai&display=swap" rel="stylesheet">
+<style>
+
+.navbar{
+    width: auto;
+    border-bottom: 1px solid #D9D9D9;
+    display: flex;
+    justify-content: space-between;
+    font-family: 'Noto Sans Thai', sans-serif;
+    padding: 0.5%;
+    align-items: center;
+}
+
+.navbarmenu{
+    display: flex;
+    height: 100%;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 8%;
+    white-space: nowrap;
+}
+
+.navbaruserlist{
+    display: flex;
+    height: 100%;
+    align-items: center;
+    justify-content: flex-end;
+    gap: 20%;
+    white-space: nowrap;
+}
+
+.profileicon{
+    display: flex;
+    height: 100%;
+    align-items: center;
+    gap: 10%;
+}
+
+a:link, a:visited {
+    color: black;
+    text-decoration: none;
+}
+a:hover{
+    color:red;
+}
+
+</style>
+<nav class="navbar">
+    <div class="navbarmenu">
+        <a href="index-admin.php"><img src="image\Logo.png" width="120rem"></a>
+        <div><a href="editproduct1.php">อัลบั้มสอด</a></div>
+        <div><a href="editproduct2.php">อัลบั้มกาว</a></div>
+        <div><a href="editproduct3.php">อัดรูป</a></div>
+        <div><a href="history.php">ประวัติลูกค้า</a></div> 
+        <div><a href="checkorderdetail.php">รายการสินค้าที่ต้องตรวจสอบ</a></div>
+        <div><a href="checkordersent.php">รายการสินค้าที่ต้องจัดส่ง</a></div>
+    </div>
+    <div class="navbaruserlist">
+        <div class="profileicon">
+            <img src="image\Profile.png" width="30rem">
+            <?php if(!isset($_SESSION['user_username'])) { ?><a href="login.php">Log in / Sign in </a><?php } ?>
+        </div>
+        <div><?php if(isset($_SESSION['user_username'])) { ?>Hello! <?php echo $_SESSION['user_username']; } ?></div>
+        <div><?php if(isset($_SESSION['user_username'])) { ?><a href="logout.php" class="linkstyle">ออกจากระบบ</a><?php } ?></div>
+    </div>
+</nav>
