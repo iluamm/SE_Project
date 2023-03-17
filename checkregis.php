89482dc1e2a65128396aacd13a0c46a6cdd01de7
iluamm <118557520+iluamm@users.noreply.github.com>
2023-03-18 04:18:07 +0700

    bug fixed register & login

--- checkregis.php
+++ checkregis.php
@@ -6,23 +6,34 @@ require_once 'connect.php';
 if(isset($_POST['regis']))
 {
     $name = $_POST['name'];
-    $username = $_POST['username'];
-    $password1 = $_POST['password'];
-    $password2 = $_POST['password2'];
+    $username = $mysqli -> real_escape_string($_POST['username']);
+    $password1 = $mysqli -> real_escape_string($_POST['password']);
+    $password2 = $mysqli -> real_escape_string($_POST['password2']);
     $user_type = "customer";
+
+    $strSQL = "SELECT * FROM username WHERE username = '". $username."'";
+    $objQuery = $mysqli -> query($strSQL); 
+    $objResult = mysqli_num_rows($objQuery);
+
     if($name=="" || $username=="" || $password1=="" || $password2==""){
-        $_SESSION['error'] = '1';
-        header("location: http://localhost/SE_Project/register.php");
+        echo "<script>";
+        echo "alert('กรุณากรอกข้อมูลให้ครบถ้วน');";
+        echo "window.history.back();";
+        echo "</script>";
+    }
+    elseif($objResult>0){
+        echo "<script>";
+        echo "alert('ชื่อผู้ใช้งานนี้ มีการใช้งานแล้ว');";
+        echo "window.history.back();";
+        echo "</script>";
     }
     elseif($password1==$password2){
-        $_SESSION['error'] = 'test';
         $sql = "INSERT INTO username (user_id,username, password,user_type)
         VALUES (NULL,'$username', '$password1', '$user_type')";
         $objQuery = $mysqli -> query($sql);
         header( "location: http://localhost/SE_Project/login.php" );
     }
     else{
-            $_SESSION["error"] = "test";
             header("location: http://localhost/SE_Project/register.php");
     }
 }
--- register.php
+++ register.php
@@ -13,8 +13,8 @@ require_once 'connect.php';
 include("navbar.php")
 ?>
 <h1 align="center">ลงชื่อเข้าใช้</h1>
-<br><?php if(isset($_SESSION['error'])) { echo $_SESSION['error']; } ?>
 <form name="frm" align="center" method="post" action="checkregis.php" enctype="multipart/from-data">
+<label>
 <input class="form-input" type="text" name="name" placeholder="ชื่อ-นามสกุล">
 <input class="form-input" type="text" name="username" placeholder="เบอร์โทรศัพท์">
 <input class="form-input" type="text" name="password" placeholder="รหัสผ่าน">
