b8fc678a0d3c981ef42efe216b99838c21c499a2
iluamm <118557520+iluamm@users.noreply.github.com>
2023-03-18 19:32:40 +0700

    fixed bug (just bug ahaha)

--- register.php
+++ register.php
@@ -14,7 +14,6 @@ include("navbar.php")
 ?>
 <h1 align="center">ลงชื่อเข้าใช้</h1>
 <form name="frm" align="center" method="post" action="checkregis.php" enctype="multipart/from-data">
-<label>
 <input class="form-input" type="text" name="name" placeholder="ชื่อ-นามสกุล">
 <input class="form-input" type="text" name="username" placeholder="เบอร์โทรศัพท์">
 <input class="form-input" type="text" name="password" placeholder="รหัสผ่าน">
