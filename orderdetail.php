29c635a092c3d63be26058622f3700562fca6775
iluamm <118557520+iluamm@users.noreply.github.com>
2023-03-19 23:41:38 +0700

    patch update 19.1

--- orderdetail.php
+++ orderdetail.php
@@ -8,6 +8,7 @@ require_once 'connect.php';
 <link rel="stylesheet" href="csscol/style.css" />
 <link rel="stylesheet" href="csscol/buttonstyle.css" />
 <link rel="stylesheet" href="csscol/tablestyle.css" />
+<link rel="stylesheet" href="csscol/inputstyle.css" />
 </head>
 <body>
 
