db2fa8f86a8472d0f9a4cfb958c5d0f4bec15eb5
iluamm <118557520+iluamm@users.noreply.github.com>
2023-03-24 03:10:59 +0700

    small patch
    forgot to change orderproduct.php

--- orderproduct1.php
+++ orderproduct1.php
@@ -18,65 +18,36 @@ include("navbar.php")
     <h3>อัลบั้มสอด</h3>
 
     <div class="menuitemlist">
-        <div class="menueachitemlist">
-            <div class="picturemenu">
-                <img src="album\elephant.jpg">
-            </div>
-            <div>
-                <p>3x2 นิ้ว</p>
-                <p class="graytext">จำนวน 64 รูป ราคา 279 บาท</p>
-            </div>
-        </div>
 
-        <div class="menueachitemlist">
-            <div class="picturemenu">
-                <img src="album\elephant.jpg">
-            </div>
-            <div>
-                <p>3x2 นิ้ว</p>
-                <p class="graytext">จำนวน 64 รูป ราคา 279 บาท</p>
-            </div>
-        </div>
-
-        <div class="menueachitemlist">
-            <div class="picturemenu">
-                <img src="album\elephant.jpg">
-            </div>
-            <div>
-                <p>3x2 นิ้ว</p>
-                <p class="graytext">จำนวน 64 รูป ราคา 279 บาท</p>
-            </div>
-        </div>
-
-        <div class="menueachitemlist">
-            <div class="picturemenu">
-                <img src="album\elephant.jpg">
-            </div>
-            <div>
-                <p>3x2 นิ้ว</p>
-                <p class="graytext">จำนวน 64 รูป ราคา 279 บาท</p>
-            </div>
-        </div>
-
-        <div class="menueachitemlist">
-            <div class="picturemenu">
-                <img src="album\elephant.jpg">
-            </div>
-            <div>
-                <p>3x2 นิ้ว</p>
-                <p class="graytext">จำนวน 64 รูป ราคา 279 บาท</p>
-            </div>
-        </div>
-
-        <div class="menueachitemlist">
-            <div class="picturemenu">
-                <img src="album\elephant.jpg">
-            </div>
-            <div>
-                <p>3x2 นิ้ว</p>
-                <p class="graytext">จำนวน 64 รูป ราคา 279 บาท</p>
-            </div>
-        </div>
+    <?php
+            $query = "SELECT * FROM promotion WHERE p_type='insert'";
+            $result = mysqli_query($mysqli, $query);
+            while($row = mysqli_fetch_row($result)) {
+                if(isset($_SESSION['user_username'])) {
+                    echo "<a href='orderproduct.php'>";
+                    echo "<div class='menueachitemlist'>";
+                    echo "<div class='picturemenu'>";
+                    echo "<img src='promotion/$row[5]'>";
+                    echo "</div>";
+                    echo "<div>";
+                    echo "<p>$row[2] นิ้ว</p>";
+                    echo "<p class='graytext'>จำนวน $row[3] รูป ราคา $row[4] บาท</p>";
+                    echo "</div>";
+                    echo "</div></a>";
+                }else{
+                    echo "<a href='login.php'>";
+                    echo "<div class='menueachitemlist'>";
+                    echo "<div class='picturemenu'>";
+                    echo "<img src='promotion/$row[5]'>";
+                    echo "</div>";
+                    echo "<div>";
+                    echo "<p>$row[2] นิ้ว</p>";
+                    echo "<p class='graytext'>จำนวน $row[3] รูป ราคา $row[4] บาท</p>";
+                    echo "</div>";
+                    echo "</div></a>";
+                    }
+            }
+        ?>
 
     </div>
 </div>
