9d84bb9e8aca92564242f674a7fcb234d04eec29
iluamm <118557520+iluamm@users.noreply.github.com>
2023-04-10 23:27:23 +0700

    fixed bug
    admin can delete promotion or album if it didn't use

--- deletealbum.php
+++ deletealbum.php
@@ -5,12 +5,18 @@ $album_id = $_GET['id'];
 $query = "SELECT * FROM album WHERE album_id = '".$album_id."'";
 $result = mysqli_query($mysqli, $query);
 $row = mysqli_fetch_row($result);
-chown("album/$row[3]",666);
 $flgDelete = unlink("album/$row[3]");
     $query = "DELETE FROM album WHERE album_id='$album_id'";
     $objQuery = $mysqli -> query($query);
-    echo "<script>";
-    echo "alert('ลบสำเร็จ');";
-    echo "window.history.back();";
-    echo "</script>";
+    if($objQuery){
+        echo "<script>";
+        echo "alert('ลบสำเร็จ');";
+        echo "window.history.back();";
+        echo "</script>";
+    }else{
+        echo "<script>";
+        echo "alert('อัลบั้มนี้มีการใช้งานอยู่');";
+        echo "window.history.back();";
+        echo "</script>";
+    }
 ?>
--- deletepro.php
+++ deletepro.php
@@ -8,8 +8,15 @@ $row = mysqli_fetch_row($result);
 $flgDelete = unlink("promotion/$row[5]");
     $query = "DELETE FROM promotion WHERE promotion_id='$promotion_id'";
     $objQuery = $mysqli -> query($query);
-    echo "<script>";
-    echo "alert('ลบสำเร็จ');";
-    echo "window.history.back();";
-    echo "</script>";
+    if($objQuery){
+        echo "<script>";
+        echo "alert('ลบสำเร็จ');";
+        echo "window.history.back();";
+        echo "</script>";
+    }else{
+        echo "<script>";
+        echo "alert('โปรโมชั่นนี้มีการใช้งานอยู่');";
+        echo "window.history.back();";
+        echo "</script>";
+    }
 ?>
